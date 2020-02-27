2a236f3b38a30fd1733457a5d12435e6d9823f73
Jefferson Brito <jefersonbritoalm@gmail.com>
2020-02-27 10:15:41 -0300

    creating repositories

---
+++ src/main/java/com/br/mastertech/mashie/repositories/MashieRepository.java
@@ -0,0 +1,7 @@
+package com.br.mastertech.mashie.repositories;
+
+import com.br.mastertech.mashie.models.Mashie;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MashieRepository extends CrudRepository<Mashie, String> {
+}
---
+++ src/main/java/com/br/mastertech/mashie/repositories/StatementRepository.java
@@ -0,0 +1,8 @@
+package com.br.mastertech.mashie.repositories;
+
+import com.br.mastertech.mashie.models.Statement;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface StatementRepository extends CrudRepository<Statement, Long> {
+}
